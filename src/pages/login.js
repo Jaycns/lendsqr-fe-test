20a47d3d0cbdd3c99008f6be0e7170b16ee96ae8
Jaycn <okonkwojude69@gmail.com>
2022-10-09 09:30:59 +0100

    login added

---
+++ src/pages/login.js
@@ -0,0 +1,45 @@
+import React, { useState, useCallback } from "react";
+import logo from "../assets/logo.svg";
+import canvas from "../assets/canvas.svg";
+import "../App.scss";
+
+function Login() {
+  const [keyShown, setKeyShown] = useState(false);
+  const handleKeyShown = useCallback(() => {
+    setKeyShown(!keyShown);
+  }, [keyShown]);
+  return (
+    <>
+      <div className="login-container">
+        <div className="img-box">
+          <img src={logo} alt="logo" className="logo" />
+          <img src={canvas} alt="logo" className="canvas" />
+        </div>
+        <div className="flex-box">
+          <div className="login-box">
+            <h1>Welcome!</h1>
+            <p>Enter details to login.</p>
+            <form>
+              <div className="form-input">
+                <input type="text" placeholder="Email" />
+              </div>
+              <div className="form-input">
+                <input
+                  type={keyShown ? "text" : "password"}
+                  placeholder="Password"
+                />
+                <div className="" onClick={handleKeyShown}>
+                  {keyShown ? <span>Hide</span> : <span>Show</span>}
+                </div>
+              </div>
+              <span>Forgot password?</span>
+              <button>Log in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
